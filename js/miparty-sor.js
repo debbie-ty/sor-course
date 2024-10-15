28f970a16a5d183ab9a9c8ba35e1e7d383964726
debbie-ty <s971435@gmail.com>
2024-10-15 16:28:43 +0800

    Add files via upload

---
+++ js/miparty-sor.js
@@ -0,0 +1,325 @@
+const courseData = {
+  title: "SoR科普系列講座",
+  weeks: [
+    {
+      week: 1,
+      date:"2024/02/18（免費試聽）",
+      title: "第01堂 SoR 概論｜SoR 科普系列講座",
+      level:0,
+      data: {
+        url:" https://reurl.cc/A4pVkd "
+      }
+    },
+    {
+      week: 2,
+      date:"2024/02/25（免費試聽）",
+      title: "第02堂 Comprehension 入門概論｜SoR 科普系列講座",
+      level:5,
+      data: {
+        url:"https://www.youtube.com/live/6YXP6LCPay4?si=1TxGv_sPvDeHam8H",
+        feedback:"https://forms.gle/nSRLFeG68zT7FqCJA"
+      }
+    },
+    {
+      week: 3,
+      date:"2024/03/03（免費試聽）",
+      title: "第03堂 Vocabulary & Grammar 入門概論｜SoR 科普系列講座",
+      level:4,
+      data: {
+        url:"https://www.youtube.com/live/7hNvFNADEDQ?si=7CSaIUOBy6B75zil",
+        feedback:"https://forms.gle/diSejSBNidBXzRaU9"
+      }
+    },
+    {
+      week: 4,
+      date:"2024/03/10（免費試聽）",
+      title: "第04堂 Fluency 入門概論｜SoR 科普系列講座",
+      level:3,
+      data: {
+        url:"https://pse.is/5p58xr",
+        feedback:"https://forms.gle/qNU6pinqAF9sNNKJA"
+      }
+    },
+    {
+      week: 5,
+      date:"2024/03/17（免費試聽）",
+      title: "第05堂 Phonics 入門概論｜SoR 科普系列講座",
+      level:2,
+      data: {
+        url:"https://pse.is/5pygkq",
+        feedback:"https://forms.gle/Pg5tQUMstQfc3etS8"
+      }
+    },
+    {
+      week: 6,
+      date:"2024/03/24（免費試聽）",
+      title: "第06堂 PA入門概論｜SoR 科普系列講座",
+      level:1,
+      data: {
+        url:"https://www.youtube.com/live/XnYP4AYXbSs?si=NFjOeYS9sOahqpu-",
+        feedback:"https://forms.gle/3pfUrN9c1U2i6Ga56"
+      }
+    },
+    {
+      week: 7,
+      date:" 2024/03/31（免費試聽）",
+      title: "第一次 SoR 五階段複習｜SoR 科普系列講座",
+      level:0,
+      data: {
+        url:"https://www.youtube.com/live/4OGMSGvTZfY?si=i31sEmPnPrAUYi6P",
+        feedback:"https://forms.gle/nbEDC9nRPTTX1XT89"
+      }
+    },
+    {
+      week: 8,
+      date:"2024/04/14",
+      title: "第08堂 SoR 第五階段 Comprehension 日常輕鬆的練習｜SoR 科普系列講座",
+      level:5,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 9,
+      date:"2024/04/28",
+      title: "第09堂 SoR 第四階段 Vocabulary & Grammar 日常輕鬆的練習｜SoR 科普系列講座",
+      level:4,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 10,
+      date:" 2024/5/5",
+      title: "第10堂 Fluency 流暢準確：日常的輕鬆練習｜SoR 科普系列講座",
+      level:3,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 11,
+      date:"2024/5/19",
+      title: "第11堂 Phonics 日常的輕鬆練習｜SoR 科普系列講座",
+      level:2,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 12,
+      date:"2024/6/16",
+      title: "第12堂 PA 日常的輕鬆練習｜SoR 科普系列講座",
+      level:1,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 13,
+      date:"2024/6/23",
+      title: "第13堂 第2次 SoR 五階段複習｜SoR 科普系列講座",
+      level:0,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 14,
+      date:"2024/6/30",
+      title: "第14堂 Comprehension 進階理論｜SoR 科普系列講座",
+      level:5,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 15,
+      date:"2024/7/7",
+      title: "第15堂 Vocabulary 進階理論｜SoR 科普系列講座",
+      level:4,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 16,
+      date:"2024/7/14",
+      title: "第16堂 Fluency 進階理論｜SoR 科普系列講座",
+      level:3,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 16.5,
+      date:"2024/7/21",
+      title: "第16.5堂 Phonics 教案分享｜SoR 科普系列講座",
+      level:2,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 17,
+      date:"2024/7/28",
+      title: "第17堂 Phonics 進階理論｜SoR 科普系列講座",
+      level:2,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 18,
+      date:"2024/8/4",
+      title: "第18堂 PA 進階理論｜SoR 科普系列講座",
+      level:1,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 19,
+      date:"2024/8/11",
+      title: "第19堂 SoR 第三次五階段複習｜SoR 科普系列講座",
+      level:0,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 20,
+      date:"2024/8/18",
+      title: "第20堂 Comprehension 對考試和檢定的幫助",
+      level:5,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 21,
+      date:"2024/9/1",
+      title: "第21堂 Vocabulary 對考試和檢定的幫助｜SoR 科普系列講座",
+      level:4,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 22,
+      date:"2024/9/8",
+      title: "第22堂 Fluency 對考試檢定的幫助 ｜SoR 科普系列講座",
+      level:3,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 23,
+      date:"2024/9/15",
+      title: "第23堂 Phonics 對考試檢定的幫助 ｜SoR 科普系列講座",
+      level:2,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 24,
+      date:"2024/9/22",
+      title: "第24堂 PA 對考試檢定的幫助｜SoR 科普系列講座",
+      level:1,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 25,
+      date:"2024/10/6",
+      title: "第25堂 從學測英文作文，釐清 SoR 腦科學的五階段學習｜SoR 科普系列講座",
+      level:0,
+      data: {
+        message:"付費解鎖"
+      }
+    },
+    {
+      week: 26,
+      date:"2024/10/13",
+      title: "第26堂 SoR 第五階 Comprehension(高階理論)｜SoR 科普系列講座",
+      level:5,
+      data: {
+        message:"付費解鎖"
+      }
+    }
+  ]
+};
+
+function renderDashboard() {
+  const courseTitle = document.getElementById('courseTitle');  
+  courseTitle.textContent = courseData.title;
+
+  const weeksGrid = document.getElementById('weeksGrid');
+  courseData.weeks.forEach(week => {
+    weeksGrid.appendChild(createLevelCard(week));
+  });
+
+
+}
+
+function createLevelCard(weekData) {
+  const card = document.createElement('div');
+  card.className = 'week-card';
+  let content = `
+        <div class="week-header">第${weekData.week}週: ${weekData.date}</div>
+        <div class="week-content level${weekData.level}">${weekData.title}</div>
+        <div class="week-data">`;
+        for (const [key, value] of Object.entries(weekData.data)) {
+          if(key == 'url') {
+            content += `<li>影片🔗：<a href="${value}" target="_blank">${value}</a></li>`;
+          } else if(key == 'feedback') {
+            content += `<li>問卷🖊️：<a href="${value}" target="_blank">${value}</a></li>`;
+          } else if(key == 'homework') {
+            content += `<li>作業 📑：<a href="${value}" target="_blank">${value}</a></li>`;
+          } else if(key == 'message') {
+            content += `<span>${value}</li>`;
+          }else {
+            content += `<li>補充教材：<a href="${value}" target="_blank">${value}</a></li>`;
+          }
+        }   
+  content += `</div>`;
+
+  
+
+  card.innerHTML = content;
+  return card;
+}
+
+
+function searchItems() {
+  // 取得輸入框中的值
+  const input = document.getElementById('search-input');
+  const filter = input.value.toLowerCase();
+  
+  // 取得清單中的所有項目
+  const weekCards = document.querySelectorAll('.week-card');
+
+  // 循環檢查每個項目是否符合關鍵字
+  weekCards.forEach(card => {
+    const header = card.querySelector('.week-header');
+    const content = card.querySelector('.week-content');
+    
+    const headerText = header.textContent || header.innerText;
+    const contentText = content.textContent || content.innerText;
+
+    const combinedText = (headerText + ' ' + contentText).toLowerCase();
+
+    if (combinedText.includes(filter.toLowerCase())) {
+        card.classList.remove('hide');
+    } else {
+        card.classList.add('hide');
+    }
+      
+  });
+}
+
+renderDashboard();
+
